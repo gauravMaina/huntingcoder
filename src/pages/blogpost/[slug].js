06bcfded5752458b3bf5cd649c76f5209fa3b10d
Gaurav Jadhav <you@example.com>
2023-01-30 10:48:55 +0530

    creating blog and blogpost page

--- src/pages/blogpost/[slug].js
+++ src/pages/blogpost/[slug].js
@@ -1,13 +1,20 @@
+import React from 'react';
 import { useRouter } from 'next/router'
-import React from 'react'
+import styles from '../../styles/BlogPost.module.css';
 
-function Slug() {
-    const router = useRouter()
-    const {slug} =router.query
-    console.log('slug',router.query)
-  return (
-    <div>{slug}</div>
-  )
-}
+const Slug = () => {
+    const router = useRouter();
+    const { slug } = router.query;
+    return <div className={styles.container}>
+        <main className={styles.main}>
+            <h1>Title of the page {slug}</h1>
+            <hr />
+            <div>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem nulla repudiandae sint facilis, sunt corrupti numquam id illo. Ut deserunt animi iste voluptatum!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus maxime rem earum repudiandae, cum possimus quae assumenda nulla culpa. Odit architecto repellendus non, unde recusandae placeat nisi perferendis quod nesciunt! Dolorum sapiente et sint consequuntur earum blanditiis iusto reprehenderit molestiae quia eligendi? Exercitationem, officia nobis!
+            </div>
+        </main>
+    </div>;
+};
 
-export default Slug
+export default Slug;
