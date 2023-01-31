afa738c7a988d27447159192332ce0a9137f5e52
Gaurav Jadhav <you@example.com>
2023-01-31 16:27:27 +0530

    designing for about and contact us

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import Navbar from 'components/Navbar'
 
 export default function App ({ Component, pageProps }) {
+  console.log("pageProps",pageProps)
   return (
     <>
       <Navbar />
